0c7af8800c7c066306cdb58b30590afa935d238e
Luigi <Luisx@hotmail.co.uk>
2020-04-17 13:19:49 +0100

    Update Hello.py
    modified

--- Hello.py
+++ Hello.py
@@ -1,3 +1,11 @@
-print "Content-Type: text/plain"
-print ""
-print "Ciao PugleseWeb, I have deployed using Openshift!"
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers["Content-Type"] = "text/plain"
+        self.response.write("Hi PublieseWeb, this is my openshift deployment!")
+
+routes = [('/', MainPage)]
+
+my_app = webapp2.WSGIApplication(routes, debug=True)
