113eb39b0864d73df74075d36d50e7e5d728d3ed
Luigi <Luisx@hotmail.co.uk>
2020-04-20 16:32:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-import os
-from Flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-	return "Hello PublieseWeb, this Luigi's OpenShift!"
-
-@app.route('/how are you')
 def hello():
-	return 'I am good, how about you?'
-
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8080)
+    return "Hello World!"
